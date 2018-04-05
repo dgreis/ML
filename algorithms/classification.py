544d23720ca81f6f1214c58d626295c84f040978
David Greis <2243125+dgreis@users.noreply.github.com>
2018-04-05 16:17:30 +0800

    Added some more classifiers from sklearn

--- algorithms/classification.py
+++ algorithms/classification.py
@@ -1,6 +1,31 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
 
 class Multinomial_Classifier(LogisticRegression):
 
     def __init__(self,**kwargs):
         LogisticRegression.__init__(self,**kwargs)
+
+class Random_Forest_Classifier(RandomForestClassifier):
+
+    def __init__(self,**kwargs):
+        RandomForestClassifier.__init__(self,**kwargs)
+
+
+class MultiLayer_Perceptron_NN_Classifier(MLPClassifier):
+
+    def __init__(self,**kwargs):
+        MLPClassifier.__init__(self,**kwargs)
+
+class Basic_SVM_Classifier(SVC):
+
+    def __init__(self,**kwargs):
+        SVC.__init__(self,**kwargs)
+
+class Gaussian_Naive_Bayes_Classifier(GaussianNB):
+
+    def __init__(self,**kwargs):
+        GaussianNB.__init__(self,**kwargs)
