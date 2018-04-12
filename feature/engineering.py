45d04aea79106331b048171a7a9f21eb028a0993
David Greis <2243125+dgreis@users.noreply.github.com>
2018-04-12 20:29:55 +0800

    fixed column naming for basis_expansion of degree 2. Also sharpened matching inclusion pattern to only match from beginning of feature name

--- feature/engineering.py
+++ feature/engineering.py
@@ -40,8 +40,9 @@ class TransformChain(Manipulator):
                 transform_class = getattr(engineering_module, transformer_name)
                 transformer = transform_class(model_config)
                 X_touch, X_untouched = transformer.split(X_mat, working_features)
-                X_touched = transformer.fit_transform(X_touch)
-                X_transform, updated_col_map = transformer.combine_and_reindex(X_touched, X_untouched, working_features)
+                X_touched, new_feat_names = transformer.fit_transform(X_touch, working_features)
+                X_transform, updated_col_map = transformer.combine_and_reindex(X_touched, X_untouched
+                                                                    ,working_features, new_feat_names)
                 self._set_working_features(updated_col_map)
                 if train == True:
                     self._output_features(transformer_name)
@@ -101,21 +102,25 @@ class Transformer:
             X_touch = X_mat
         else:
             for pattern in inclusion_patterns:
-                pat_include_columns = filter(lambda x: pattern in x, col_names)
-                include_columns = include_columns + pat_include_columns
+                len_pat = len(pattern)
+                pattern_begin_cols = filter(lambda x: x[0:len_pat] == pattern, col_names)
+                include_columns = include_columns + pattern_begin_cols
             touch_indices = [int(inv_working_features[col_name]) for col_name in include_columns]
             untouched_indices = list(set(col_indices).difference(set(touch_indices)))
             X_untouched = X_mat.loc[:,untouched_indices]
             X_touch = X_mat.loc[:,touch_indices]
         return X_touch, X_untouched
 
-    def fit_transform(self,X_touch):
-        X_touched = self.base_transformer.fit_transform(X_touch)
+    def fit_transform(self, X_touch, working_features):
+        X_touched = self.base_transformer.fit_transform(X_touch, )
+        orig_tcol_idx = X_touch.columns
+        Xt_feat_names = self.gen_new_column_names(orig_tcol_idx, working_features)
         if type(X_touched) != pd.core.frame.DataFrame: #TODO: fix this when I move out of pandas
-            rdf = pd.DataFrame(X_touched,columns=X_touch.columns)
+            rdf = pd.DataFrame(X_touched)
         else:
             rdf = X_touched
-        return rdf
+        assert len(Xt_feat_names) == X_touched.shape[1]
+        return rdf, Xt_feat_names
 
     def store_output(self,X_mat,output_dir): #TODO: When I need to output filter, make this an abstract method in new Manipulator class
         model_name = self.model_name
@@ -123,13 +128,12 @@ class Transformer:
         output_file_name = slugify(model_name + '-' + transform_name)
         X_mat.to_csv(output_dir + '/' + output_file_name + '.txt', header=False, index=False, sep='\t')  # TODO: fix this when I move out of pandas
 
-    def combine_and_reindex(self, Xt_df, Xut_df, col_map):
-        Xt_feat_names = self.gen_new_column_names(Xt_df, col_map)
+    def combine_and_reindex(self, Xt_df, Xut_df, working_features, Xt_feat_names):
         untouched_indices = Xut_df.columns.tolist()
         new_col_map = dict()
         ni = 0
         for oi in untouched_indices:
-            col_name = col_map[oi]
+            col_name = working_features[oi]
             new_col_map[ni] = col_name
             ni += 1
         Xut_df.columns = range(len(untouched_indices))
@@ -148,7 +152,7 @@ class Transformer:
         assert ni == X_transform.shape[1]
         return X_transform, new_col_map
 
-    def gen_new_column_names(self,Xt_df,col_map):
+    def gen_new_column_names(self, orig_tcol_idx, working_features):
         raise NotImplementedError
 
 class basis_expansion(Transformer):
@@ -157,17 +161,28 @@ class basis_expansion(Transformer):
         Transformer.__init__(self, model_config)
         self.set_base_transformer(PolynomialFeatures(**self.kwargs))
 
-    def gen_new_column_names(self, Xt_df):
-        Xt_feat_names = list()
-        num_poly_features = Xt_df.shape[1]
-        for i in range(num_poly_features):
-            #base_feature_name = col_map[ind]
-            #poly_feature_name = base_feature_name + '**' + str(i+1)
-            poly_feature_name = 'polyfeature..' + str(i)  ##TODO: Make this name meaningful. Note this won't filter properly
-            Xt_feat_names.append(poly_feature_name)       ## using exclusion_patterns
-        assert len(Xt_feat_names) == Xt_df.shape[1]
+    def gen_new_column_names(self, orig_tcol_idx, working_features):
+        tuples = list()
+        for i in range(len(orig_tcol_idx)):
+            b = orig_tcol_idx[i]
+            rem_list = orig_tcol_idx[i:]
+            t = [(working_features[b],working_features[r_i]) for r_i in rem_list]
+            tuples = tuples + t
+        stiched_feature_names = [self.stich(x) for x in tuples]
+        orig_tcol_names = [working_features[idx] for idx in orig_tcol_idx]
+        Xt_feat_names = orig_tcol_names + stiched_feature_names
+        if getattr(self.base_transformer,'include_bias'):
+            Xt_feat_names = ['bias'] + Xt_feat_names
+        if getattr(self.base_transformer,'interaction_only'):
+            Xt_feat_names = filter(lambda x: '**' not in x, Xt_feat_names)
         return Xt_feat_names
 
+
+    def stich(self,tuple):
+        if tuple[0] == tuple[1]:
+            return tuple[0] + '**2'
+        else:
+            return tuple[0] + '_' + tuple[1]
 
 class normalize(Transformer):
 
@@ -175,10 +190,10 @@ class normalize(Transformer):
         Transformer.__init__(self, model_config)
         self.set_base_transformer(Normalizer(**self.kwargs))
 
-    def gen_new_column_names(self,Xt_df,col_map):
+    def gen_new_column_names(self, orig_tcol_idx, working_features):
         Xt_feat_names = list()
-        for idx in Xt_df.columns.tolist():
-            base_feature_name = col_map[idx]
+        for idx in orig_tcol_idx:
+            base_feature_name = working_features[idx]
             norm_feature_name = 'norm(' + base_feature_name + ')'
             Xt_feat_names.append(norm_feature_name)
         return Xt_feat_names
@@ -190,10 +205,10 @@ class standard_scale(Transformer):
         Transformer.__init__(self,model_config)
         self.set_base_transformer(StandardScaler(**self.kwargs))
 
-    def gen_new_column_names(self,Xt_df,col_map):
+    def gen_new_column_names(self, orig_tcol_idx, working_features):
         Xt_feat_names = list()
-        for idx in Xt_df.columns.tolist():
-            base_feature_name = col_map[idx]
+        for idx in orig_tcol_idx:
+            base_feature_name = working_features[idx]
             norm_feature_name = 'scaled(' + base_feature_name + ')'
             Xt_feat_names.append(norm_feature_name)
         return Xt_feat_names
@@ -204,7 +219,7 @@ class pca(Transformer):
         Transformer.__init__(self, model_config)
         self.set_base_transformer(PCA(**self.kwargs))
 
-    def gen_new_column_names(self,Xt_df,col_map):
+    def gen_new_column_names(self, orig_tcol_idx, working_features):
         num_comps = self.base_transformer.n_components
         new_col_list = list()
         for i in range(num_comps):
