6c414f4434f6c4a1c5806b527242c91f094a0f28
David Greis <2243125+dgreis@users.noreply.github.com>
2018-05-06 13:56:08 +0800

    migrate base_transformers to their own file

--- feature/engineering.py
+++ feature/engineering.py
@@ -5,8 +5,9 @@ import itertools
 
 import pandas as pd
 import numpy as np
-import numpy.random as npr
 
+from feature.base_transformers import InvOneHotEncoder, Interpolator, LeaveOneOutEncoder, Truncator, Deleter, Identity, \
+    Sampler
 from manipulator import ManipulatorChain, Manipulator
 from algorithms.wrapper import Wrapper
 from utils import flip_dict, load_inv_column_map, load_clean_input_file_filepath
@@ -16,7 +17,6 @@ from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from statsmodels.nonparametric.smoothers_lowess import lowess
-from scipy.interpolate import interp1d
 
 from django.utils.text import slugify
 
@@ -45,19 +45,6 @@ class TransformChain(ManipulatorChain):
                 updated_transformations = updated_transformations + [transformation]
         super(TransformChain,self).__init__(updated_transformations, model_config, project_settings,original_columns)
         self.transformations = updated_transformations
-
-    # def fit(self,X_mat,y,dataset_name):
-    #     log_prefix = "[" + dataset_name + "]"
-    #     transformations = self.transformations
-    #     i = 1
-    #     for d in transformations:
-    #         transformer_name = d.keys()[0]
-    #         #print "\t" + log_prefix + " Performing feature engineering (" + str(i) + '/' + str(
-    #         #    len(transformations)) + "): " + transformer_name
-    #         transformer = d[transformer_name]['initialized_transformer']
-    #         X_touch, X_untouched, y_touch, y_untouched = transformer.split(X_mat, y)
-    #         transformer.fit(X_touch,y_touch)
-    #         i += 1
 
     def transform(self,X_mat,y,dataset_name,fit_transform=False):
         X_mat_start = X_mat.copy()
@@ -433,47 +420,6 @@ class loo_encoding(Transformer):
         return [new_feature_name]
 
 
-class LeaveOneOutEncoder:
-
-    def __init__(self):
-        pass
-
-    def fit(self, X_touch, y_touch):
-        loo_vals = dict()
-        mean_val_dict = dict()
-        y_ser = pd.Series(y_touch,index=X_touch.index)
-        for j in X_touch.columns:
-            j_idx = X_touch[X_touch.loc[:, j] > 0].index  #take all rows that belong to category j
-            yj_dict = dict(y_ser.loc[j_idx])
-            cat_sum = sum(yj_dict.values())
-            cat_len = len(yj_dict)
-            if cat_len > 0:
-                mean_val_dict[j] = cat_sum / cat_len
-            else:
-                mean_val_dict[j] = y_ser.mean()
-            for i in j_idx:
-                if cat_len > 1:
-                    y_loo_catmean = (cat_sum - yj_dict[i]) / (cat_len - 1)    #for each row in j_idx, calc mean w/o row value
-                else:
-                    y_loo_catmean = (y_ser.sum() - yj_dict[i]) / (len(y_ser) - 1)
-                loo_vals[i] = y_loo_catmean
-        self.loo_means = pd.Series(loo_vals)
-        self.mean_val_dict = mean_val_dict
-
-    def transform(self, X_touch, y_touch, dataset_name):
-        if dataset_name == 'train':
-            loo_means = self.loo_means
-            assert len(loo_means) == len(X_touch)
-            assert not pd.isnull(loo_means).any()
-            return pd.DataFrame(self.loo_means), y_touch
-        else:
-            assert len(self.mean_val_dict) == X_touch.shape[1]
-            mean_val_dict = self.mean_val_dict
-            cat_means = X_touch.idxmax(1).apply(lambda x: mean_val_dict[x])
-            assert not pd.isnull(cat_means).any()
-            return pd.DataFrame(cat_means,index=X_touch.index), y_touch
-
-
 class interpolate(Transformer):
     """Example in models.yaml file:
     Models:
@@ -506,7 +452,7 @@ class interpolate(Transformer):
         lowess_x = list(zip(*fitted_lowess))[1]
 
         # run scipy's interpolation. There is also extrapolation I believe
-        f = Interpolator(lowess_x, lowess_y,bounds_error=False,**interp1d_kwargs)
+        f = Interpolator(lowess_x, lowess_y, bounds_error=False, **interp1d_kwargs)
 
         self.set_base_transformer(f)
 
@@ -517,13 +463,6 @@ class interpolate(Transformer):
         new_feature_name = 'interp(' + old_feature_name + ')'
         return [new_feature_name]
 
-class Interpolator(interp1d):
-
-    def __init__(self,x,y,**kwargs):
-        super(Interpolator,self).__init__(x,y,fill_value='extrapolate',**kwargs)
-
-    def transform(self,x):
-        return self(x)
 
 class as_numeric(TransformChain):
     """Example in models.yaml file:
@@ -585,7 +524,7 @@ class ind_as_numeric(Transformer):
         kwargs = dict()
         if ind_as_numeric_entry.has_key('val_map'):
             kwargs['val_map'] = ind_as_numeric_entry['val_map']
-        self.set_base_transformer(InvOneHotEncoder(self.touch_indices, self.prior_features,**kwargs))
+        self.set_base_transformer(InvOneHotEncoder(self.touch_indices, self.prior_features, **kwargs))
 
 
     def gen_new_column_names(self, touch_indices, prior_features):
@@ -598,55 +537,6 @@ class ind_as_numeric(Transformer):
     def transform(self, X_touch, y_touch, dataset_name):
         return self.base_transformer.transform(X_touch,y_touch,dataset_name)
 
-class InvOneHotEncoder:
-
-    def __init__(self,touch_indices, prior_features,val_map=None):
-        self.prior_features = prior_features
-        self.touch_indices = touch_indices
-
-        if val_map is not None:
-            self.val_map = val_map
-
-        idx_val_map = dict()
-
-        for ti in touch_indices:
-            prior_feature_col = prior_features[ti]
-            value = prior_feature_col.split('_')[1]
-            idx_val_map[ti] = value
-            if val_map is not None:
-                try:
-                    assert value in val_map.keys()
-                except AssertionError:
-                    print value + " is not in val_map for feature " + prior_feature_col.split('_')[0]
-                    raise Exception
-                idx_val_map[ti] = float(val_map[value])
-            else:
-                try:
-                    idx_val_map[ti] = float(idx_val_map[ti])
-                except ValueError:
-                    print "Specify a val_map for feature " + prior_feature_col.split('_')[0]
-                    raise Exception
-                    #TODO: include this in Exception message
-
-        self.idx_val_map = idx_val_map
-
-    def fit(self,X,y):
-        pass
-
-    def transform(self,X_touch,y_touch,dataset_name):
-        #Pandas dependent
-        max_col = X_touch.idxmax(1)
-        idx_val_map = self.idx_val_map
-        num_column = max_col.apply(lambda x: idx_val_map[x])
-        if hasattr(self,'val_map'):
-            val_map = self.val_map
-            inv_val_map = flip_dict(val_map)
-            for val in inv_val_map:
-                try:
-                    assert val in num_column.values
-                except AssertionError:
-                    print '\t' + str(val) + " not in column for dataset:" + dataset_name + ". Possibly Tweak val_map in as_numeric transform"
-        return pd.DataFrame(num_column,index=X_touch.index), y_touch
 
 class sample(Transformer):
     """Example in models.yaml file:
@@ -715,24 +605,6 @@ class sample(Transformer):
     def gen_new_column_names(self, touch_indices, prior_features):
         return prior_features
 
-class Sampler:
-
-    def __init__(self,upsample=False):
-        self.touch_indices = None
-        self.untouched_indices = None
-        self.upsample_flag = upsample
-
-    def transform(self,X_touch,y_touch):
-        touch_indices = self.touch_indices
-        untouched_indices = self.untouched_indices
-        sample_size = len(untouched_indices)
-        sampled_idx = npr.choice(touch_indices,size=sample_size)
-        X_touch_dict = X_touch.to_dict(orient='index')
-        new_x_rows = [X_touch_dict[ri] for ri in sampled_idx]
-        X_touched = pd.DataFrame(new_x_rows,index=sampled_idx)
-        y_touch_dict = dict(pd.Series(y_touch,index=touch_indices))
-        y_touched = [y_touch_dict[ri] for ri in sampled_idx]
-        return X_touched, y_touched
 
 class predict(Transformer):
 
