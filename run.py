ede61aa8e0c279c9ab8ccc190dc9449c83c7f61c
David Greis <2243125+dgreis@users.noreply.github.com>
2019-02-23 06:30:25 +0530

    cosmetic print statement adjustments. had to import future print to string together statements on one line

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import fractions
 
 from utils import *
@@ -13,19 +15,19 @@ def main():
     if not all_clean_input_files_exist(project_settings):
         prep_data = importlib.import_module('projects.' + project_settings['project_name'] + '.src.' + 'prep_data')
         prep_data.main()
-        print "Data Built for project: " + global_settings['current_project']
+        print("Data Built for project: " + global_settings['current_project'])
     project_settings['working_files'] = project_settings['clean_input_files'].copy()
 
     model_configs = load_model_configs(project_settings)
     models = configure_algorithms(model_configs, project_settings)
     num_models = len(models)
 
-    print "\nFit ML models for project: " + project_settings['project_name']
-    print "Number of models to fit: " + str(num_models)
+    print("\nFit ML models for project: " + project_settings['project_name'])
+    print( "Number of models to fit: " + str(num_models))
     i = 1
     report = Report(project_settings)
     for model_name in models:
-        print "\nFitting model (" + str(i) + "/" + str(num_models) + "): " + model_name
+        print("\nFitting model (" + str(i) + "/" + str(num_models) + "): " + model_name)
         model_config = model_configs['Models'][model_name]
         model_config['model_name'] = model_name
         manager = Manager(model_config,project_settings)
@@ -34,13 +36,13 @@ def main():
         cv = CrossValidator(model_config, project_settings)
         cv_num_folds = det_num_cv_folds(model_config, project_settings)
         if cv_num_folds > 1:
-            print "Next Step: estimate generalization error for some metrics using CV"
+            print("Next Step: estimate generalization error for some metrics using CV")
             entries = cv.perform_cross_validation(data, model)
             report.add_entries_to_report(entries)
         else:
-            print "CV folds set to less than 2. No CV performed"
+            print("CV folds set to less than 2. No CV performed. ",end="")
         #TODO: Figure out 'remaining metrics' apply to regression. If not, this part could be skipped. Answer: Yes, only regression
-        print "Next Step: estimate remaining metrics using validation set."
+        print("Next Step: estimate remaining metrics using validation set.")
         X_train_val, y_train_val = manager.load_clean_datasets('train_val', project_settings)['train_val']
         if cv_num_folds < 2:
             implied_folds = fractions.Fraction.from_float(model_config['train_val_split']).limit_denominator(10).denominator
@@ -51,12 +53,14 @@ def main():
         ind_dev, ind_val = manager.return_fold_dev_val_ind(model_config['fold_i'])
         X_train, y_train = X_train_val.loc[ind_dev, :], pd.Series(y_train_val).loc[ind_dev].tolist()
         X_train_p, y_train_p = manager.fit_transform(X_train, y_train, 'train')
-        model.fit(X_train_p, y_train_p)
-        print 'Model Fit. Next Step: Perform Model Evaluation'
         X_val, y_val = X_train_val.loc[ind_val, :], pd.Series(y_train_val).loc[ind_val].tolist()
         X_val_p, y_val_p = manager.transform(X_val, y_val, 'val')
         assert X_train_p.shape[1] == X_val_p.shape[1]
         assert len(y_val_p) == len(y_val)
+        print("Validation dataset finalized. Training with " + str(len(X_train_p)) + " samples. Validation data with " +\
+              str(len(X_val_p)) + " samples. Model with " + str(X_val_p.shape[1]) + " features. Now fitting model... ",end="")
+        model.fit(X_train_p, y_train_p)
+        print('Model Fit.\nNext Step: Perform Model Evaluation')
         y_pred = model.predict(X_val_p)
         if model.gen_output_flag:
             model.gen_output()
@@ -102,9 +106,9 @@ def main():
                 report_row['content'] = content
                 report.add_entries_to_report(report_row)
         i += 1
-    print "All Models Fit and Evaluated. Writing Report"
+    print("All Models Fit and Evaluated. Writing Report")
     report.write_report()
-    print "\nReport written to: " + project_settings['repo_loc'] +'/' + project_settings['project_name'] + '/evaluation/report.html'
+    print("\nReport written to: " + project_settings['repo_loc'] +'/' + project_settings['project_name'] + '/evaluation/report.html')
 
 if __name__ == "__main__":
     main()
