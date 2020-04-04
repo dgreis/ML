df82fde4689b41c136c4dcb3d424918be85b7b66
David Greis <2243125+dgreis@users.noreply.github.com>
2020-04-04 07:54:31 +0300

    Implement voting classifier

--- algorithms/algoutils.py
+++ algorithms/algoutils.py
@@ -5,6 +5,9 @@ from algorithms.wrapper import Wrapper
 def configure_algorithm(model_config, project_settings):
     base_algorithm = model_config['base_algorithm']
     base_algo_class = get_algo_class(base_algorithm)
+    if ('.').join([base_algo_class.__module__,base_algo_class.__name__]) in project_settings['overwritten_algos']:
+        print("Don't use original " + base_algorithm + " sklearn implementation. Check algorithms in package for custom package implementation instead")
+        raise Exception
     if 'algorithms' not in base_algorithm:
         base_algo_instance = Wrapper('final_algorithm', base_algo_class, model_config, project_settings)  #Wrapper is used for algos implemented by others, i.e. sklearn
     else:
--- algorithms/classification.py
+++ algorithms/classification.py
@@ -3,12 +3,40 @@ from utils import flip_dict, load_inv_column_map
 from .wrapper import Wrapper
 from .common import DecisionTree
 from sklearn import tree
+from sklearn import ensemble
+from algorithms.algoutils import get_algo_class
 
+#TODO: Do I need this below? Or is it redundant?
 class DecisionTreeClassifier(DecisionTree):
 
     def __init__(self, wrapper_id, model_config, project_settings):
         base_algo_class = tree.DecisionTreeClassifier
         super(DecisionTreeClassifier, self).__init__(wrapper_id, base_algo_class, model_config, project_settings)
+
+class VotingClassifier(Wrapper):
+    """Custom implementation of sklearn.ensemble.VotingClassifier because component classifiers need
+    to be initialized first.
+
+    """
+    def __init__(self, wrapper_id, model_config, project_settings):
+        base_algo_class = ensemble.VotingClassifier
+        estimators_raw = model_config['keyword_arg_settings']['estimators']
+        if type(estimators_raw) == dict:
+            estimators_final = list()
+            for k in estimators_raw:
+                try:
+                    est_algo_class = get_algo_class(estimators_raw[k]['base_algorithm'])
+                except TypeError:
+                    assert 1 == 0
+                est_algo_kwargs = estimators_raw[k]['keyword_arg_settings']
+                est_algo_instance = est_algo_class(**est_algo_kwargs)
+                estimators_final.append((k,est_algo_instance))
+        elif type(estimators_raw) == list:
+            estimators_final = estimators_raw
+        else:
+            raise Exception
+        model_config['keyword_arg_settings']['estimators'] = estimators_final
+        super(VotingClassifier, self).__init__(wrapper_id, base_algo_class, model_config, project_settings)
 
 # class Bagged_Classifier(Wrapper):
 #
