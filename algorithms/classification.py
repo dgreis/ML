e1a7f4143ab5539ea4d80e614d17b98e7ee8033d
David Greis <2243125+dgreis@users.noreply.github.com>
2020-04-06 07:52:17 +0300

    add AugGradientBoostingClassifier with output. Also change how gen_output works

--- algorithms/classification.py
+++ algorithms/classification.py
@@ -1,20 +1,24 @@
+import pandas as pd
+import math
 
 from utils import flip_dict, load_inv_column_map
 from .wrapper import Wrapper
 from .common import DecisionTree
+from django.utils.text import slugify
 from sklearn import tree
 from sklearn import ensemble
 from sklearn import multiclass
 from sklearn import base
 from algorithms.algoutils import get_algo_class
+from sklearn_gbmi import h_all_pairs
 
-#TODO: Do I need this below? Or is it redundant?
 class DecisionTreeClassifier(DecisionTree):
-
+    """Utility class to help common.DecisionTree know what base algo to use"""
     def __init__(self, wrapper_id, model_config, project_settings):
         base_algo_class = tree.DecisionTreeClassifier
         super(DecisionTreeClassifier, self).__init__(wrapper_id, base_algo_class, model_config, project_settings)
 
+#TODO: Change all algo initialization to handle estimator (or multiple estimators) that require their own intialization first
 class VotingClassifier(Wrapper):
     """Custom implementation of sklearn.ensemble.VotingClassifier because component classifiers need
     to be initialized first.
@@ -54,3 +58,60 @@ class OutputCodeClassifier(Wrapper):
             estimator_final = est_algo_instance
         model_config['keyword_arg_settings']['estimator'] = estimator_final
         super(OutputCodeClassifier, self).__init__(wrapper_id, base_algo_class, model_config, project_settings)
+
+class AugGradientBoostingClassifier(Wrapper):
+
+    def __init__(self, wrapper_id, model_config, project_settings):
+        base_algo_class = ensemble.GradientBoostingClassifier
+        super(AugGradientBoostingClassifier, self).__init__(wrapper_id, base_algo_class, model_config, project_settings)
+
+    def fit(self,X,y):
+        self.X = X
+        super().fit(X,y)
+
+    def feature_importances(self):
+        fitted_algo = self.base_algorithm
+        fi = pd.Series(fitted_algo.feature_importances_)
+        features = self.features
+        feature_names = [features[i] for i in range(len(features))]
+        fi.index = feature_names
+        artifact_dir = self.artifact_dir
+        model_name = self.model_name
+        fi.sort_values(ascending=False).to_csv(artifact_dir +'/' + slugify(model_name) + '-feature-importances.txt'
+                  ,header=False
+                  )
+
+    def interaction_strengths(self,feature_importance_thresh):
+        """
+        Example Usage:
+          Testing GBM Ints:
+            base_algorithm: algorithms.classification.AugGradientBoostingClassifier
+            output:
+              - interaction_strengths:
+                  feature_importance_thresh: 0.5 ( top x% if less than 1, num
+                                                    to be considered if > 1 AND int)
+        """
+        fitted_algo = self.base_algorithm
+        fi = pd.Series(fitted_algo.feature_importances_).sort_values(ascending=False)
+        if feature_importance_thresh <= 1:
+            incl_feat_idx = fi.head(math.ceil(feature_importance_thresh*len(fi))).index
+        elif type(feature_importance_thresh) != int:
+            raise Exception
+        elif feature_importance_thresh > 1:
+            incl_feat_idx = fi.head(feature_importance_thresh).index
+        else:
+            raise Exception
+        X = self.X
+        raw_output_dict = h_all_pairs(fitted_algo,X.loc[:,incl_feat_idx])
+        features = self.features
+        output_dict = dict()
+        for o in raw_output_dict:
+            key = tuple([features[i] for i in o])
+            output_dict[key] = raw_output_dict[o]
+        df = pd.DataFrame.from_dict(output_dict, orient='index',columns=['val'])
+        df.sort_values('val',ascending=False,inplace=True)
+        artifact_dir = self.artifact_dir
+        model_name = self.model_name
+        df.to_csv(artifact_dir +'/' + slugify(model_name) + '-interaction-strengths.txt'
+                  ,header=False
+                  )
--- algorithms/wrapper.py
+++ algorithms/wrapper.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import importlib
 
 from feature.manipulator import Manipulator
 from django.utils.text import slugify
@@ -9,12 +10,10 @@ class Wrapper(Manipulator):
 
     def __init__(self, wrapper_id, base_algorithm_class, model_config, project_settings):
         kwargs = model_config['keyword_arg_settings']
-        other_options = model_config['other_options']
         self.model_name = model_config['model_name']
         self.base_algorithm = base_algorithm_class(**kwargs)
-        if 'gen_output' in other_options:
-            arg_val = other_options['gen_output']
-            self.gen_output_flag = arg_val
+        if len(model_config['output']) > 0:
+            self.gen_output_flag = True
         else:
             self.gen_output_flag = False
         super(Wrapper, self).__init__(wrapper_id, model_config, project_settings)
@@ -37,6 +36,25 @@ class Wrapper(Manipulator):
         return y_pred
 
     def gen_output(self):
+        model_config = self.model_config
+        output_options = model_config['output']
+        if len(output_options) == 0:
+            self.gen_default_output()
+        else:
+            wrapper_module_name = self.__module__
+            wrapper_module = importlib.import_module(wrapper_module_name)
+            wrapper_class = getattr(wrapper_module, self.__class__.__name__)
+            for oo in output_options:
+                if type(oo) == str:
+                    method_name = oo
+                    kwargs = {}
+                elif type(oo) == dict:
+                    assert len(oo) == 1
+                    method_name = list(oo.keys())[0]
+                    kwargs = oo[method_name]
+                getattr(wrapper_class, method_name)(self,**kwargs)
+
+    def gen_default_output(self):
         y_pred = self.y_pred
         artifact_dir = self.artifact_dir
         model_name = self.model_name
